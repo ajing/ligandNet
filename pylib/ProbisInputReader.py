12ee6d3912b6cd64531080eefcdb2b4a9720046e
ajing <ajingnk@gmail.com>
2013-09-10 11:09:02 -0400

    check amino acid ligand

--- pylib/ProbisInputReader.py
+++ pylib/ProbisInputReader.py
@@ -3,3 +3,35 @@
     Author: ajing
     Date:   9/10/2013
 """
+
+class Probis:
+    def __init__( self ):
+        self.PROBISFILE = "/users/ajing/ligandNet/pylib/ProbisInput.txt"
+        self.COLNAME = [
+                "INDEX",
+                "MOADINDEX",
+                "BIOUNIT",
+                "LIGANDID",
+                "PROTEINCHAIN",
+                "BINDINGSITE",
+                "BINDINGSITESIZE",
+                "HEADER",
+                "ISHEADER"
+                ]
+        self.probisdict = self.ProbisInputParser( self.PROBISFILE )
+
+    def ProbisInputParser( self, infile ):
+        ProbisDict = dict()
+        for each in self.COLNAME:
+            ProbisDict[ each ] = []
+        for line in open(infile):
+            content = line.strip().split("\t")
+            total_len = len(content)
+            if not total_len == len( self.COLNAME):
+                raise "file corrupted as different colnames"
+            for each in range( total_len ):
+                ProbisDict[ self.COLNAME[each] ].append( content[each] )
+        return ProbisDict
+
+if __name__ == "__main__":
+    test = Probis()
--- pylib/aminoacidException.py
+++ pylib/aminoacidException.py
@@ -5,5 +5,22 @@
 
 """
 
+from ProbisInputReader import Probis
+
 __AMINO_ACID__ = [ "ALA", "CYS", "ASP", "GLU", "PHE", "GLY", "HIS", "ILE", "LYS", "LEU", "MET", "ASN", "PRO", "GLN", "ARG", "SER", "THR", "VAL", "TRP", "TYR"]
 
+def numberOfAminoAcidLigand():
+    probis   = Probis()
+    ligandID = probis.probisdict["LIGANDID"]
+    n = 0
+    for index in range(len(ligandID)):
+        if "_" in ligandID[index]:
+            continue
+        ligandName = ligandID[index].split(".")[0]
+        if ligandName in __AMINO_ACID__:
+            n = n + 1
+            print probis.probisdict["BIOUNIT"][index] + "," + ligandID[index] + "," + probis.probisdict["BINDINGSITESIZE"][index]
+    print n
+
+if __name__ == "__main__":
+    numberOfAminoAcidLigand()
